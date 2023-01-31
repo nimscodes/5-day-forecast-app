5c75cbfd23a9d5a1d120c8722910f7a54668d114
nimscodes <princenimako28@gmail.com>
2023-01-31 15:11:49 +0000

    worked on functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,69 @@ const FORECAST = document.getElementById('forecast');
 const APIKEY = "88f2e9603106d9234412cf3c047c4822"
 
 
-function renderTodaysWeather(userInput) {
-    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=Leicester&appid=${APIKEY}`
+function renderForecast(city) {
+    const queryURL = `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&cnt=40&appid=${APIKEY}`
     $.ajax({
         url: queryURL
-    }).then(response => {
+    }).then(function(response){
         console.log(response);
-        const cityName = response.name;
-        const todaysDate = moment().format("DD/MM/YYYY");
-        const weatherIcon = response.weather[0].icon;
+        const forecast = response.list;
+        const cityName = response.city.name;
+        const todaysDate = moment(new Date()).format("DD/MM/YYYY");
+        console.log(todaysDate);
+        const forecastArr = [];
+        for (let i = 0; i < forecast.length; i += 8) {
+            forecastArr.push(forecast[i]);
+        }
+
+        console.log(forecastArr);
+        const tempInDegrees = forecastArr[0].main.temp;
+        const humidity = forecastArr[0].main.humidity;
+        const windSpeed = forecastArr[0].wind.speed;
+        const weatherIcon = forecastArr[0].weather[0].icon;
         const iconurl = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
-        const tempInDegrees = response.main.temp - 273.15;
-        const humidity = response.main.humidity;
-        const windSpeed = response.wind.speed;
 
         $('#today').append(`
-        <div class="card">
-            <div class="card-body">
-            <h5 class="card-title">${cityName} (${todaysDate})<img src=${iconurl} alt="weather icon"></h5>
-            <p class="card-text">Temp: ${tempInDegrees.toFixed(2)}&#8451;</p>
-            <p class="card-text">Wind: ${windSpeed}KPH</p>
-            <p class="card-text">Humidity: ${humidity}%</p>
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">${cityName} (${todaysDate})<img src=${iconurl} alt="weather icon"></h5>
+                <p class="card-text">Temp: ${tempInDegrees}&#8451;</p>
+                <p class="card-text">Wind: ${windSpeed}KPH</p>
+                <p class="card-text">Humidity: ${humidity}%</p>
+                </div>
             </div>
-        </div>
         `)
+        
+        for (let i = 0; i < forecastArr.length; i++) {
+            const new_date = moment(new Date()).add(i, 'd').format("DD-MM-YYYY");
+            const weatherIcon = forecastArr[i].weather[0].icon;
+            const iconurl = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+            const tempInDegrees = forecastArr[i].main.temp;
+            const humidity = forecastArr[i].main.humidity;
+            
+            $('#forecast').append(`
+                <div class="card bg-primary mr-2 mb-2 p-2 text-white" style = "width: 11rem;">
+                    <div class="card-body">
+                    <h5 class="card-title">${new_date}</h5>
+                    <p class="card-text"><img src=${iconurl} alt="weather icon"></p>
+                    <p class="card-text">Temp: ${tempInDegrees}&#8451;</p>
+                    <p class="card-text">Humidity: ${humidity}%</p>
+                    </div>
+                </div>
+            `)
+        }
+
+    
+
+        // console.log(moment);
     })
 }
 
-renderTodaysWeather();
+function renderHistoryButtons(){}
+
 
 SEARCH.addEventListener('click', (event) => {
     event.preventDefault();
     const userInput = $('#search-input').val().trim();
-    renderTodaysWeather(userInput);
+    renderForecast(userInput);
 })
